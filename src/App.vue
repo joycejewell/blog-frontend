fc0abfc9618708be0e6974d1fd010d41a6be53f8
joycejewell <joyceroop@gmail.com>
2022-08-10 19:53:59 -0600

    updated to clear errors and prep for heroku

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,55 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Dropdown
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
   <nav>
     <router-link to="/">Home</router-link>
     |
@@ -21,7 +72,8 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;
+  background-image: url("https://www.toptal.com/designers/subtlepatterns/uploads/white_wall_hash.png");
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -4,6 +4,7 @@ export default {
   data: function () {
     return {
       posts: [],
+      currentpost: [],
     };
   },
   created: function () {
@@ -23,13 +24,35 @@ export default {
 <template>
   <div>
     <h1>All Posts</h1>
-    <div v-for="post in posts" v-bind:key="post.id">
+    <div class="row">
+      <div class="col-sm-4" v-for="post in posts" v-bind:key="post.id" v-on:mouseover="currentpost = post">
+        <div class="card mb-4" v-bind:class="{ selected: post == currentpost }">
+          <img class="m-auto card-img-top" v-bind:src="post.image_url" v-bind:alt="post.title" />
+          <div class="card-body">
+            <h5 class="card-title">{{ post.title }}</h5>
+            <p class="card-text">Description {{ post.description }}</p>
+            <a v-bind:href="`/posts/${posts.id}`" class="btn btn-primary">More Info</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div v-for="post in posts" v-bind:key="post.id">
       <h2>Title: {{ post.title }}</h2>
       <img v-bind:src="post.image" v-bind:alt="post.title" />
       <p>Description: {{ post.body }}</p>
       <a v-bind:href="`/posts/${post.id}`">More info</a>
-    </div>
+    </div> -->
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card img {
+  object-fit: cover;
+  height: 250px;
+}
+.selected {
+  /* color: white; */
+  background-color: #e6e6e6;
+  transition: background-color 1s ease;
+}
+</style>
